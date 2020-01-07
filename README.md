d4e709b9ce192a034669c1801bbe8148c2838497
Jasper van Merle <jaspervmerle@gmail.com>
2020-01-07 07:58:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Battlecode 2020
+
+The Battlecode 2020 bot of team "camel_case".
---
+++ src/com/jaspervanmerle/battlecode2020/RobotPlayer.java
@@ -0,0 +1,19 @@
+package com.jaspervanmerle.battlecode2020;
+
+import battlecode.common.Clock;
+import battlecode.common.RobotController;
+
+@SuppressWarnings("unused")
+public class RobotPlayer {
+  @SuppressWarnings("unused")
+  public static void run(RobotController rc) {
+    while (true) {
+      try {
+        Clock.yield();
+      } catch (Exception e) {
+        System.out.println("Exception in robot #" + rc.getID() + " (" + rc.getType() + ")");
+        e.printStackTrace();
+      }
+    }
+  }
+}
